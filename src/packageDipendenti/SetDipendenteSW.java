89f2fca9747cb801fb458edb9d92db927d0d14c8
MoroSurface <moro8699@hotmail.it>
2017-10-15 20:41:40 +0200

    Modificata La Classe Per la Modifica Dipendente

--- src/packageDipendenti/SetDipendenteSW.java
+++ src/packageDipendenti/SetDipendenteSW.java
@@ -6,13 +6,17 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Iterator;
-import javax.swing.BoxLayout;
+import java.util.Vector;
+
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableModel;
 
 import main.Principale;
 import packageImpianti.Impianti;
@@ -21,23 +25,40 @@ import packageImpianti.Impianto;
 public class SetDipendenteSW extends JFrame {
 	
 	private static final long serialVersionUID = -8722179695766788480L;
+	protected  final int R_NOME =0, R_COGNOME =1, R_IMPIANTO =2, R_ASSUNZ =3, R_RECAPITI =4;
 	private int rowAttuale = 0;
-	protected JTextField nome, cognome;
 	protected JLabel matricola, recapiti;
 	protected static JLabel impianto;
 	protected JButton ok, cancel, assegnaImpianto, modRecapiti;
 	protected Dipendente d;
+	private JTable datiDipendente;
+	private Vector<String> nomeRiga, dipendente, header;
+	private Vector<Vector<String>> dati;
+	private DefaultTableModel modelloDatiDipendente;
 	
 	public SetDipendenteSW (Dipendente d){
 		
 		this.d = d;
-		nome = new JTextField(20);
-		nome.setText(d.getNome());
-		cognome = new JTextField(20);
-		cognome.setText(d.getCognome());
-		matricola = new JLabel(d.getMatricola());
-		impianto = new JLabel(d.labelImpianto());
-		recapiti = new JLabel(ElencoTelefonicoDipendenti.cercaTelefoni(d));
+		
+		inizializzaDatiDipendente();
+		
+		modelloDatiDipendente = new DefaultTableModel() {
+
+			private static final long serialVersionUID = 1798174918491824698L;
+
+			@Override
+		    public boolean isCellEditable(int row, int column) {
+		        if (row == R_RECAPITI && column == 1) return false;		       
+				if (column != 0) return true;
+		        return false;
+		    }		
+		};
+		
+		modelloDatiDipendente.setDataVector(dati, header);
+		datiDipendente = new JTable(modelloDatiDipendente);
+		datiDipendente.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		JScrollPane scroll = new JScrollPane(datiDipendente);
+		
 		rowAttuale = Principale.posizioneAttualeTable();
 		
 		JPanel centro = new JPanel();
@@ -50,42 +71,54 @@ public class SetDipendenteSW extends JFrame {
 		getContentPane().add(sud, BorderLayout.SOUTH);
 		
 		nord.add(new JLabel("DATI DIPENDENTE " + d.getMatricola()));
-		
-		centro.setLayout(new BoxLayout(centro, BoxLayout.Y_AXIS));
-		JPanel pnlCognome = new JPanel();
-		pnlCognome.add(new JLabel("COGNOME : "));	
-		pnlCognome.add(cognome);	
-		centro.add(pnlCognome);
-		JPanel pnlNome = new JPanel();
-		pnlNome.add(new JLabel("NOME : "));	
-		pnlNome.add(nome);
-		centro.add(pnlNome);
-		JPanel pnlImpianto = new JPanel();
-		assegnaImpianto = new JButton("Assegna");
-		assegnaImpianto.addActionListener(new ApriAssegnaImpianto() );
-		pnlImpianto.add(new JLabel("IMPIANTO : "));
-		pnlImpianto.add(impianto);
-		pnlImpianto.add(assegnaImpianto);
-		centro.add(pnlImpianto);
-		JPanel pnlRecapiti = new JPanel();
-		pnlRecapiti.add(new JLabel("RECAPITI TELEFONICI : "));
-		pnlRecapiti.add(recapiti);
-		modRecapiti = new JButton("Gestisci Recapiti");
-		modRecapiti.addActionListener(new Recapiti());
-		pnlRecapiti.add(modRecapiti);
-		centro.add(pnlRecapiti);
+		centro.setLayout(new BorderLayout());
+		centro.add(scroll, BorderLayout.CENTER);
 		
 		ok = new JButton("OK");
-		ok.addActionListener(new Ok());
+		ok.addActionListener(new Ok(d));
 		cancel = new JButton("Cancel");
 		cancel.addActionListener(new Cancel());
+		modRecapiti = new JButton("Gestione Recapiti");
+		modRecapiti.addActionListener(new Recapiti());
+		
 		sud.add(ok);
 		sud.add(cancel);
+		sud.add(modRecapiti);
 		
 		setSize(500,350);		
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation ((int)(dim.getWidth()-this.getWidth())/2, (int)(dim.getHeight()-this.getHeight())/2);
 		setVisible(true);
+		
+	}
+	
+	public void inizializzaDatiDipendente() {
+		
+		header = new Vector<String>();
+		nomeRiga = new Vector<String>();
+		dipendente = new Vector<String>();
+		dati = new Vector<>();
+		
+		header.addElement("Attributo");
+		header.addElement("Valore");
+		
+		nomeRiga.add(R_NOME, "Nome");
+		dipendente.add(R_NOME, d.getNome());
+		nomeRiga.add(R_COGNOME, "Cognome");
+		dipendente.add(R_COGNOME, d.getCognome());
+		nomeRiga.add(R_IMPIANTO, "Impianto");
+		dipendente.add(R_IMPIANTO, d.labelImpianto());
+		nomeRiga.add(R_ASSUNZ, "Assunzione");
+		dipendente.add(R_ASSUNZ, d.getDataAssunzione().toString());
+		nomeRiga.add(R_RECAPITI, "Recapiti");
+		dipendente.add(R_RECAPITI, ElencoTelefonicoDipendenti.cercaTelefoni(d));
+		
+		for(int i =0; i<nomeRiga.size(); i++) {
+			Vector<String> temp = new Vector<>();
+			temp.addElement(nomeRiga.elementAt(i));
+			temp.addElement(dipendente.elementAt(i));
+			dati.addElement(temp);
+		}
 		
 	}
 	
@@ -156,18 +189,26 @@ public class SetDipendenteSW extends JFrame {
 	
 	
 	class Ok implements ActionListener{
-
+		
+		Dipendente dipendente;
+		public Ok(Dipendente dip) {
+			dipendente = dip;
+		}
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			Dipendente d = new Dipendente(nome.getText(), cognome.getText(), matricola.getText());
+			
+			String nome = (String) datiDipendente.getValueAt(R_NOME, 1),
+					cognome = (String) datiDipendente.getValueAt(R_COGNOME, 1);
+			
+			Dipendente d = new Dipendente(nome, cognome, dipendente.getMatricola());
 			Iterator<Dipendente> it = ListaDipendenti.getListaDipendenti().iterator();
 			while (it.hasNext()){
 				Dipendente dip = it.next();
 				if (dip.equals(d)){
-					dip.setNome(nome.getText());
-					dip.setCognome(cognome.getText());
+					dip.setNome(nome);
+					dip.setCognome(cognome);
 					Principale.getModelloTable().setValueAt
-						(cognome.getText() + " " + nome.getText() , rowAttuale, Principale.COLONNA_COGNOME_NOME);
+						(cognome + " " + nome , rowAttuale, Principale.COLONNA_COGNOME_NOME);
 					Principale.getModelloTable().setValueAt
 						(dip.getImpiantoDiAppartenenza() , rowAttuale, Principale.COLONNA_IMPIANTO);
 				}
